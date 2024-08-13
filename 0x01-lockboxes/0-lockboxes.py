103d56d5c59b5be931dc796de7732f463ef89550
Lain0x0 <lainyamato03@gmail.com>
2024-08-13 17:25:44 +0000

    lockboxes-0

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''Write a method that determines if all the boxes can be opened.'''
+
+
+def canUnlockAll(boxes):
+    '''You have n number of locked boxes in front of you. Each box is numbered
+    sequentially from 0 to n - 1 and each box may contain keys
+    to the other boxes.
+    '''
+    n = len(boxes)
+    seen_boxes = set([0])
+    unseen_boxes = set(boxes[0]).difference(set([0]))
+
+    while (len(unseen_boxes) > 0):
+        boxIdx = unseen_boxes.pop()
+        if (not boxIdx or boxIdx >= n or boxIdx < 0):
+            continue
+        if (boxIdx not in seen_boxes):
+  
+            unseen_boxes = unseen_boxes.union(boxes[boxIdx])
+            seen_boxes.add(boxIdx)
+
+
+    return n == len(seen_boxes)
