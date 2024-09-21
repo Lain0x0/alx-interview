3b575a1aaaaf45b8e5bb907f64fb8da82485a8c0
Lain0x0 <lainyamato03@gmail.com>
2024-09-21 14:04:41 +0000

    fix it

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+/**
+ * Star wars API
+ * Alx-interview
+ */
+
+const request = require('request');
+
+const fetch = (url) => new Promise((resolve, reject) => {
+  request(url, (err, _, body) => {
+    if (err) reject(err);
+    else resolve(JSON.parse(body));
+  });
+});
+
+const movieId = process.argv[2];
+if (!movieId) {
+  console.error('Usage: ./star_wars_characters.js <movie_id>');
+  process.exit(1);
+}
+
+fetch(`https://swapi.dev/api/films/${movieId}/`)
+  .then(film => Promise.all(film.characters.map(fetch)))
+  .then(characters => characters.forEach(character => console.log(character.name)))
+  .catch(console.error);
